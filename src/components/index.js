1fa1091fe12fadee28d032ef9519323d5bc48f34
fahad <fjkiani@icloud.com>
2020-12-28 03:22:56 -0500

    commit

---
+++ src/components/index.js
@@ -0,0 +1,9 @@
+import Layout from "./layout"
+import Hero from "./hero/Hero"
+import About from "./About"
+import Projects from "./Projects"
+import Survey from "./Survey"
+import Slider from "./reviewSlider/Slider"
+import Algolia from "./Algolia"
+import GridProjects from "./GridProjects"
+export { Layout, Hero, About, Projects, Survey, Slider, Algolia, GridProjects }
